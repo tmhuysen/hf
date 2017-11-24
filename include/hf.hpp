40c9785b425e1f90729bf6ad6cbc6cc1e416e70b
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-11-24 14:38:00 +0100

    Added wrapper header file.é

---
+++ include/hf.hpp
@@ -0,0 +1,12 @@
+#ifndef HF_HF_HPP
+#define HF_HF_HPP
+
+
+// This header file acts as a collective header file
+
+#include "SCF_functions.hpp"
+#include "SCFSolver.hpp"
+#include "version.hpp"
+
+
+#endif //HF_HF_HPP
