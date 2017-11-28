82131f76fc7da5342c7db1d237a7263366586a07
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-11-28 15:03:02 +0100

    Removed unnecessary CMake commands.

--- cmake/ConfigureLibrary.cmake
+++ cmake/ConfigureLibrary.cmake
@@ -4,8 +4,6 @@
 # Include this project's headers
 target_include_directories(${LIBRARY_NAME} PRIVATE ${PROJECT_INCLUDE_FOLDER})
 
-# Include the Eigen3 headers
-target_link_libraries(${LIBRARY_NAME} PUBLIC Eigen3::Eigen)
 
 # Include the libint wrapper
 target_include_directories(${LIBRARY_NAME} PUBLIC ${libwrp_INCLUDE_DIRS})
