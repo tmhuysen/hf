3661b17b6976806decba8e976a1414623cf553c8
Laurent Lemmens <laurent.lemmens@ugent.be>
2017-11-30 08:13:18 +0100

    Changed namespace to rhf and classes to RHF.

--- src/RHF.cpp
+++ src/RHF.cpp
@@ -1,12 +1,12 @@
-#include "SCFSolver.hpp"
-#include "SCF_functions.hpp"
+#include "RHF.hpp"
+#include "RHF_functions.hpp"
 
 
 /** Constructor based on a given libwrp::Basis and an SCF-cycle threshold
  *
  *      This automatically starts the restricted SCF procedure
  */
-hf::SCFSolver::SCFSolver(libwrp::Basis& basis, double threshold):
+rhf::RHF::RHF(libwrp::Basis& basis, double threshold):
     basis(basis), threshold(threshold)
 {
     assert(this->basis.molecule.nelec % 2 == 0);  // We have only implemented a restricted Hartree-Fock algorithm
@@ -26,7 +26,7 @@ hf::SCFSolver::SCFSolver(libwrp::Basis& basis, double threshold):
     //  S should be positive definite
     Eigen::GeneralizedSelfAdjointEigenSolver<Eigen::MatrixXd> gsaes0 (H_core, basis.S);
     Eigen::MatrixXd C = gsaes0.eigenvectors();
-    Eigen::MatrixXd P = hf::calculate_P(C, this->basis.molecule.nelec);
+    Eigen::MatrixXd P = rhf::calculate_P(C, this->basis.molecule.nelec);
 
     // Initialize the loop parameters
     bool converged = false;
@@ -34,7 +34,7 @@ hf::SCFSolver::SCFSolver(libwrp::Basis& basis, double threshold):
 
     while ((! converged) || iteration_counter > this->MAX_NO_ITERATIONS) {
         // Calculate the G-matrix
-        Eigen::MatrixXd G = hf::calculate_G(P, basis.tei);
+        Eigen::MatrixXd G = rhf::calculate_G(P, basis.tei);
 
         // Calculate the Fock matrix
         Eigen::MatrixXd F = H_core + G;
@@ -45,7 +45,7 @@ hf::SCFSolver::SCFSolver(libwrp::Basis& basis, double threshold):
 
         // Calculate an improved density matrix P from the improved coefficient matrix C
         Eigen::MatrixXd P_previous = P; // We will store the previous density matrix
-        P = hf::calculate_P(C, this->basis.molecule.nelec);
+        P = rhf::calculate_P(C, this->basis.molecule.nelec);
 
         // Check for convergence on the density matrix P
         if ((P - P_previous).norm() <= this->threshold) {
@@ -59,7 +59,7 @@ hf::SCFSolver::SCFSolver(libwrp::Basis& basis, double threshold):
             std::cout << "The SCF algorithm has converged after " << iteration_counter << " iterations.\n" << std::endl;
 
             // After the calculation has converged, calculate the energy as the sum of the electronic energy and the internuclear repulsion energy
-            this->energy = hf::calculate_electronic_energy(P, H_core, F) + this->basis.molecule.internuclear_repulsion();
+            this->energy = rhf::calculate_electronic_energy(P, H_core, F) + this->basis.molecule.internuclear_repulsion();
 
             // Furthermore, add the orbital energies and the coefficient matrix to (this)
             this->orbital_energies = gsaes.eigenvalues();
