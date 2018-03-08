e92d3c950b2643b31e2a3dfe7283fe197628a73c
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-03-07 19:07:42 -0500

    Refactor the RHF class.

--- include/RHF.hpp
+++ include/RHF.hpp
@@ -14,9 +14,10 @@ namespace rhf {
 
 class RHF {
 private:
-    static const size_t MAX_NO_ITERATIONS = 128;
-    const double threshold;  // convergence threshold for the SCF procedure
+    static const size_t MAX_NUMBER_OF_SCF_CYCLES = 128;
+    const double scf_threshold;  // convergence threshold for the SCF procedure
     const libwint::AOBasis& ao_basis;
+    const size_t N;  // number of electrons
 
     Eigen::VectorXd orbital_energies;  // energies of the spatial orbitals (i.e. eigenvalues of the Fock operator)
     Eigen::MatrixXd C_canonical;  // coefficient matrix linking the spatial orbitals to the underlying basis set
@@ -24,39 +25,38 @@ private:
                                   // the coefficient matrix is canonical, which means that the Fock matrix in this basis is diagonal
 
     double electronic_energy;  // the converged energy
-    double total_energy;
 
 
     // Methods
     /**
-     *  Given a coefficient matrix @param: C, and the number of electrons N, calculate the RHF density matrix P
+     *  Given a coefficient matrix @param: C, and the number of electrons N, calculate the RHF AO density matrix P
      */
     Eigen::MatrixXd calculateP(const Eigen::MatrixXd& C) const;
 
     /**
-     *  Given the density matrix @param: P, and the two-electron integrals @param g in chemist's notation, calculate the RHF G-matrix
+     *  Given the RHF AO density matrix @param: P, and the two-electron integrals @param g in chemist's notation, calculate the RHF G-matrix
      */
     Eigen::MatrixXd calculateG(const Eigen::MatrixXd& P, const Eigen::Tensor<double, 4>& g) const;
 
-    /** Calculate the RHF electronic energy based on the density matrix P, the core Hamiltonian H_core and the Fock matrix F
-     *
+    /**
+     *  Calculate the RHF electronic energy based on the RHF AO density matrix @param: P, the core Hamiltonian @param: H_core and the Fock matrix @param: F
      */
-    double calculateElectronicEnergy(const Eigen::MatrixXd& P, const Eigen::MatrixXd& H_core, const Eigen::MatrixXd& F);
+    double calculateElectronicEnergy(const Eigen::MatrixXd& P, const Eigen::MatrixXd& H_core, const Eigen::MatrixXd& F) const;
+
 
 
 public:
     // Constructors
     /**
-     *  Constructor based on a given libwint::AOBasis @param: ao_basis and an SCF-cycle @param: scf_threshold
+     *  Constructor based on a given libwint::AOBasis @param: ao_basis, a number of electrons @param: N and an SCF-cycle @param: scf_threshold
      */
-    RHF(const libwint::AOBasis& ao_basis, double scf_threshold);
+    RHF(const libwint::AOBasis& ao_basis, size_t N, double scf_threshold);
 
 
     // Getters
     Eigen::VectorXd get_orbital_energies() const { return this->orbital_energies; }
     Eigen::MatrixXd get_C_canonical() const { return this->C_canonical; }
     double get_electronic_energy() const { return this->electronic_energy; }
-    double get_total_energy() const { return this->total_energy; }
 
 
     // Methods
@@ -64,7 +64,6 @@ public:
      *  Solve the restricted Hartree-Fock equations (i.e. the Roothaan-Hall equations)
      */
     void solve();
-
 
     /**
      *  Given a number of spatial orbitals @param: K and a number of electrons @param: N, calculated the index of the HOMO in the restricted case
--- src/RHF.cpp
+++ src/RHF.cpp
@@ -1,76 +1,201 @@
 #include "RHF.hpp"
-#include "RHF_functions.hpp"
 
 
-/** Constructor based on a given libwint::Basis and an SCF-cycle threshold
- *
- *      This automatically starts the restricted SCF procedure
+namespace hf {
+namespace rhf {
+
+
+/*
+ *  PRIVATE METHODS
  */
-hf::rhf::RHF::RHF(libwint::Basis& basis, double threshold):
-    basis(basis), threshold(threshold)
+
+/**
+ *  Given a coefficient matrix @param: C, and the number of electrons N, calculate the RHF AO density matrix P
+ */
+Eigen::MatrixXd RHF::calculateP(const Eigen::MatrixXd& C) const {
+
+    size_t K = this->ao_basis.calculateNumberOfBasisFunctions();
+
+    // Construct the occupancy matrix, i.e. the density matrix in SO basis
+    Eigen::MatrixXd O = Eigen::MatrixXd::Zero (K, K);
+    O.topLeftCorner(this->N/2, this->N/2) = 2 * Eigen::MatrixXd::Identity (this->N/2, this->N/2);
+
+
+    // P is the density matrix in AO basis
+    return libwint::transformations::transform_SO_to_AO(O, C);
+}
+
+
+/**
+ *  Given the RHF AO density matrix @param: P, and the two-electron integrals @param g in chemist's notation, calculate the RHF G-matrix
+ */
+Eigen::MatrixXd RHF::calculateG(const Eigen::MatrixXd& P, const Eigen::Tensor<double, 4>& g) const {
+
+    // We will first have to convert the Eigen::MatrixXd P to an Eigen::Tensor<double, 2> P_tensor, as contractions are only implemented for Eigen::Tensors
+    Eigen::TensorMap<Eigen::Tensor<double, 2>> P_tensor (P.data(), P.rows(), P.cols());
+
+    // Specify the contraction pairs
+    // To calculate G, we must perform two double contractions
+    //      1. (mu nu|rho lambda) P(lambda rho)
+    Eigen::array<Eigen::IndexPair<int>, 2> direct_contraction_pair = {Eigen::IndexPair<int>(3, 0), Eigen::IndexPair<int>(2, 1)};
+    //      2. -0.5 (mu lambda|rho nu) P(lambda rho)
+    Eigen::array<Eigen::IndexPair<int>, 2> exchange_contraction_pair = {Eigen::IndexPair<int>(1, 0), Eigen::IndexPair<int>(2, 1)};
+
+    // Calculate both contractions (and incorporate prefactors)
+    Eigen::Tensor<double, 2> direct_contraction = g.contract(P_tensor, direct_contraction_pair);
+    Eigen::Tensor<double, 2> exchange_contraction = -0.5 * g.contract(P_tensor, exchange_contraction_pair);
+
+    // The previous contractions are Eigen::Tensor<double 2> instances. In order to calculate the G matrix, we will convert them back into Eigen::MatrixXd instances.
+    Eigen::Map<Eigen::MatrixXd> G1 (direct_contraction.data(), direct_contraction.dimension(0), direct_contraction.dimension(1));
+    Eigen::Map<Eigen::MatrixXd> G2 (exchange_contraction.data(), exchange_contraction.dimension(0), exchange_contraction.dimension(1));
+
+    // The result is the sum of both contractions, since prefactors were already incorporated
+    return G1 + G2;
+}
+
+
+/**
+ *  Calculate the RHF electronic energy based on the RHF AO density matrix @param: P, the core Hamiltonian @param: H_core and the Fock matrix @param: F
+ */
+double RHF::calculateElectronicEnergy(const Eigen::MatrixXd& P, const Eigen::MatrixXd& H_core, const Eigen::MatrixXd& F) const {
+
+    // First, calculate the sum of H_core and F (this saves a contraction)
+    Eigen::MatrixXd Z = H_core + F;
+
+    // Convert the matrices Z and P to an Eigen::Tensor<double, 2> P_tensor, as contractions are only implemented for Eigen::Tensors
+    Eigen::TensorMap<Eigen::Tensor<double, 2>> P_tensor (P.data(), P.rows(), P.cols());
+    Eigen::TensorMap<Eigen::Tensor<double, 2>> Z_tensor (Z.data(), P.rows(), P.cols());
+
+    // Specify the contraction pair
+    // To calculate the electronic energy, we must perform a double contraction
+    //      0.5 P(nu mu) Z(mu nu)
+    Eigen::array<Eigen::IndexPair<int>, 2> contraction_pair = {Eigen::IndexPair<int>(0, 1), Eigen::IndexPair<int>(1, 0)};
+
+    // Calculate the double contraction (with prefactor 0.5)
+    Eigen::Tensor<double, 0> contraction = 0.5 * P_tensor.contract(Z_tensor, contraction_pair);
+
+    // As the double contraction of two matrices is a scalar (a tensor of rank 0), we can access the value as (0).
+    return contraction(0);
+}
+
+
+
+/*
+ *  CONSTRUCTORS
+ */
+
+/**
+ *  Constructor based on a given libwint::AOBasis @param: ao_basis, a number of electrons @param: N and an SCF-cycle @param: scf_threshold
+ */
+RHF::RHF(const libwint::AOBasis& ao_basis, size_t N, double scf_threshold) :
+    scf_threshold (scf_threshold),
+    ao_basis (ao_basis),
+    N (N)
 {
-    assert(this->basis.molecule.nelec % 2 == 0);  // We have only implemented a restricted Hartree-Fock algorithm
 
-    // Automatically start the SCF procedure
-    std::cout << "\nStarting a Hartree-Fock calculation." << std::endl;
-    libint2::initialize();
+    if ((this->N % 2) != 0) {
+        throw std::invalid_argument("You supplied an odd number of electrons to the RHF procedure.");
+    }
+}
 
-    // Compute all integrals in the AO basis
-    basis.compute_integrals();
+
+
+/*
+ *  PUBLIC METHODS
+ */
+
+/**
+ *  Solve the restricted Hartree-Fock equations (i.e. the Roothaan-Hall equations)
+ */
+void RHF::solve() {
 
     // Calculate H_core
-    Eigen::MatrixXd H_core = basis.T + basis.V;
+    Eigen::MatrixXd H_core = this->ao_basis.get_T() + this->ao_basis.get_V();
 
     // Solve the generalized eigenvalue problem for H_core to obtain a guess for the density matrix P
     //  H_core should be self-adjoint
     //  S should be positive definite
-    Eigen::GeneralizedSelfAdjointEigenSolver<Eigen::MatrixXd> gsaes0 (H_core, basis.S);
+    Eigen::GeneralizedSelfAdjointEigenSolver<Eigen::MatrixXd> gsaes0 (H_core, this->ao_basis.get_S());
     Eigen::MatrixXd C = gsaes0.eigenvectors();
-    Eigen::MatrixXd P = rhf::calculate_P(C, this->basis.molecule.nelec);
+    Eigen::MatrixXd P = this->calculateP(C);
 
     // Initialize the loop parameters
     bool converged = false;
     size_t iteration_counter = 1;
 
-    while ((! converged) || iteration_counter > this->MAX_NO_ITERATIONS) {
+    while ((! converged) || iteration_counter > this->MAX_NUMBER_OF_SCF_CYCLES) {
         // Calculate the G-matrix
-        Eigen::MatrixXd G = rhf::calculate_G(P, basis.tei);
+        Eigen::MatrixXd G = this->calculateG(P, this->ao_basis.get_g());
 
         // Calculate the Fock matrix
         Eigen::MatrixXd f_AO = H_core + G;
 
         // Solve the Roothaan equation (generalized eigenvalue problem)
-        Eigen::GeneralizedSelfAdjointEigenSolver<Eigen::MatrixXd> gsaes (f_AO, basis.S);
+        Eigen::GeneralizedSelfAdjointEigenSolver<Eigen::MatrixXd> gsaes (f_AO, this->ao_basis.get_S());
         C = gsaes.eigenvectors();
 
         // Calculate an improved density matrix P from the improved coefficient matrix C
         Eigen::MatrixXd P_previous = P; // We will store the previous density matrix
-        P = rhf::calculate_P(C, this->basis.molecule.nelec);
+        P = this->calculateP(C);
 
         // Check for convergence on the density matrix P
-        if ((P - P_previous).norm() <= this->threshold) {
+        if ((P - P_previous).norm() <= this->scf_threshold) {
 
             // After the SCF procedure, we end up with canonical spatial orbitals, i.e. the Fock matrix should be diagonal in this basis
             // Let's check if this is the case, within double float precision
-            Eigen::MatrixXd f_MO = libwint::transform_AO_to_SO(f_AO, C);  // FIXME: we can use the libwint function for this
+            Eigen::MatrixXd f_MO = libwint::transformations::transform_AO_to_SO(f_AO, C);
             assert(f_MO.isDiagonal());
 
             converged = true;
             std::cout << "The SCF algorithm has converged after " << iteration_counter << " iterations.\n" << std::endl;
 
-            // After the calculation has converged, calculate the energy as the sum of the electronic energy and the internuclear repulsion energy
-            this->energy = rhf::calculate_electronic_energy(P, H_core, f_AO) + this->basis.molecule.internuclear_repulsion();
+            // After the calculation has converged, calculate the electronic energy
+            this->electronic_energy = this->calculateElectronicEnergy(P, H_core, f_AO);
 
             // Furthermore, add the orbital energies and the coefficient matrix to (this)
             this->orbital_energies = gsaes.eigenvalues();
             this->C_canonical = C;
         }
-
-        // Update the iteration number
-        iteration_counter ++;
+        else {  // not converged yet
+            iteration_counter ++;
+        }
 
     } // SCF cycle loop
 
-    libint2::finalize();
+    // If we reach more than this->MAX_NO_ SCF cycles, the system is considered not to be converging
+    throw std::runtime_error("The SCF procedure did not converge.");
+}
 
-} // constructor
+
+/**
+ *  Given a number of spatial orbitals @param: K and a number of electrons @param: N, calculated the index of the HOMO in the restricted case
+ */
+size_t RHF::HOMO_index(size_t K, size_t N) {
+    if (N % 2 != 0) {
+        throw std::invalid_argument("The unrestricted case is not supported.");
+    }
+
+    if (N > 2 * K) {
+        throw std::invalid_argument("Cannot place that many electrons N in K spatial orbitals.");
+    }
+
+    return N / 2 - 1;  // Need to subtract 1 because computer indices start at 0
+}
+
+
+/**
+ *  Given a number of spatial orbitals @param: K and a number of electrons @param: N, calculated the index of the LUMO in the restricted case
+ */
+size_t RHF::LUMO_index(size_t K, size_t N) {
+    if (N >= 2 * K) {
+        throw std::invalid_argument("There is no LUMO for the given amount of electrons N and spatial orbitals K");
+    }
+
+    return RHF::HOMO_index(K, N) + 1;
+}
+
+
+
+}  // namespace rhf
+}  // namespace hf
+
