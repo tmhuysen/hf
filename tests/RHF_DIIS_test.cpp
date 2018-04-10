df41c9b680f02e17e826d843cd8fceeb5579e719
Tm Huysen <Tobias.Huysentruyt@Ugent.be>
2018-04-10 23:22:05 +0200

    added convergence test

--- tests/RHF_DIIS_test.cpp
+++ tests/RHF_DIIS_test.cpp
@@ -221,3 +221,18 @@ BOOST_AUTO_TEST_CASE ( lumo ) {
     // In this case, K=7 and N=10, so the index of the LUMO should be 5
     BOOST_CHECK_EQUAL(rhf.LUMOIndex(), 5);
 }
+
+BOOST_AUTO_TEST_CASE ( covergence_test ) {
+
+    // Test to see far apart NO converges
+
+    // Do our own RHF calculation
+    libwint::Molecule lih ("../tests/ref_data/NO.xyz",1);
+    libwint::AOBasis ao_basis (lih, "STO-6G");
+    ao_basis.calculateIntegrals();
+
+    hf::rhf::RHF rhf (lih, ao_basis, 1.0e-06);
+    // DIIS should converge
+    BOOST_CHECK_NO_THROW(rhf.solve(hf::rhf::solver::SCFSolverType::DIIS));
+
+}
