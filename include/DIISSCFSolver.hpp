a865d9d41195a2a46785409ae5286c91ce9afce1
Tm Huysen <Tobias.Huysentruyt@Ugent.be>
2018-04-11 18:03:40 +0200

    added constrained rhf (RHFC)

--- include/DIISSCFSolver.hpp
+++ include/DIISSCFSolver.hpp
@@ -14,7 +14,6 @@ class DIISSCFSolver: public BaseSCFSolver {
 private:
     std::deque<Eigen::MatrixXd> fock_vector;  // deque of fock matrix for recombination in subspace collapse
     std::deque<Eigen::MatrixXd> error_vector;  // deque of errors for calculating the lineair recombination coefficients
-    std::deque<Eigen::MatrixXd> density_vector;  // deque of errors for calculating the lineair recombination coefficients
 
     const size_t max_error_size = 6;  // maximum size of the deques before subspace starts collapse.
 public:
@@ -23,9 +22,9 @@ public:
      *  Constructor to initialize the const S, H_core, g, calculateP, calculateG, threshold and maximum_number_of_iterations.
      */
     explicit DIISSCFSolver(const Eigen::MatrixXd S, const Eigen::MatrixXd H_core, const Eigen::Tensor<double ,4> g,
-                            const hf::DensityFunction calculateP,
-                            const hf::TwoElectronMatrixFunction calculateG,
-                            double threshold = 1e-6, size_t maximum_number_of_iterations = 128);
+                           const hf::DensityFunction calculateP,
+                           const hf::TwoElectronMatrixFunction calculateG,
+                           double threshold = 1e-6, size_t maximum_number_of_iterations = 128);
 
 
     // DESTRUCTOR
@@ -42,6 +41,9 @@ public:
      *      - @member orbital_energies
      */
     void solve() override;
+
+
+
 };
 
 
