95474f95f54f9bb3cdd673656d2877ec7c774a39
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-04-26 14:51:50 -0400

    Add copyright headers.

--- include/RHF.hpp
+++ include/RHF.hpp
@@ -1,3 +1,19 @@
+// This file is part of GQCG-hf.
+// 
+// Copyright (C) 2017-2018  the GQCG developers
+// 
+// GQCG-hf is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// GQCG-hf is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with GQCG-hf.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef HF_RHF_HPP
 #define HF_RHF_HPP
 
